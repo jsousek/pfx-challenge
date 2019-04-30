1b8503eba88c1375c1513e35612811757de8670e
Jiri Sousek <jirisousek@outlook.com>
2019-04-30 11:00:55 +0200

    setup of project

---
+++ src/main/java/com/pfx/PfxChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.pfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfxChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfxChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pfx/PfxChallengeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pfx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PfxChallengeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
